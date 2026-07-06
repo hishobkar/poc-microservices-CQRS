fb40023345f4e34db7e0a3efdc616c343889ff30
Hishobkar <abhishobkar@gmail.com>
2026-07-06 18:53:56 +0530

    Allowed cors policy

--- backend/src/ApiGateway/Program.cs
+++ backend/src/ApiGateway/Program.cs
@@ -3,6 +3,17 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowCorsPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
 builder.Services.AddOcelot(builder.Configuration);
@@ -10,5 +21,6 @@ builder.Services.AddOcelot(builder.Configuration);
 var app = builder.Build();
 
 app.UseRouting();
+app.UseCors("AllowCorsPolicy");
 await app.UseOcelot();
 app.Run();
